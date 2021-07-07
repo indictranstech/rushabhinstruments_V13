81147e2e5068a8294d211ff2b72cb160a68d59e8
Dell <you@example.com>
2021-07-07 12:00:08 +0530

    [fixes][fixtures][Removed Blanket Order FRom View fields]

--- instrument/instrument/custom_instrument/request_for_quotation/request_for_quotation.py
+++ instrument/instrument/custom_instrument/request_for_quotation/request_for_quotation.py
@@ -6,31 +6,6 @@ from frappe.utils.background_jobs import enqueue
 
 
 def on_submit(doc, method = None):
-	# file_att = []
-	# file_att = [frappe.attach_print(doc.doctype, doc.name, file_name=doc.name)]
-	# attachments = frappe.db.sql(""" SELECT file_name  FROM tabFile 
-	# 			WHERE attached_to_name = '{0}'""".format(doc.name),as_dict=1)
-	
-	# if attachments:
-	# 	for row in attachments:
-	# 		_file = frappe.get_doc("File", {"file_name": row.file_name})
-	# 		content = _file.get_content()
-	# 		if not content:
-	# 			return
-	# 		attachment_list = {'fname':row.file_name,'fcontent':content}
-	# 		file_att.append(attachment_list)
-
-	# sender = frappe.db.get_value("Email Setting",{"email_name": "Purchase Order Email"},"email_id")
-	# if doc.suppliers:
-	# 	recipient = suppliers.contact_email
-	# if recipient:
-	# 	frappe.sendmail(
-	# 		sender = sender,
-	# 		recipients = recipient,
-	# 		subject = "Request For Quotation : {0}".format(doc.name),
-	# 		message = "Request For Quotation : " + "https://uatrushabhinstruments.indictranstech.com/app/request-for-quotation/{0}".format(doc.name),
-	# 		attachments = file_att,
-	# 		)
 	send_email_without_reference_to_supplier(doc, method)
 
 def send_email_without_reference_to_supplier(doc, method):
