28cb4e2fe9ee2af3c0e95765c19d30acf512d6a7
namrataingale <namrata.i@indictrans.in>
2021-12-22 18:18:51 +0530

    [Feature][Purchase Receipt]

--- instrument/instrument/custom_instrument/purchase_receipt/purchase_receipt.js
+++ instrument/instrument/custom_instrument/purchase_receipt/purchase_receipt.js
@@ -12,14 +12,14 @@ frappe.ui.form.on("Purchase Receipt", {
 				filters:{ 'item_code': row.item_code }
 			}
 		});	
-		// //Filter Accepted warehouse
-		// frm.set_query("warehouse", "items", function(doc, cdt, cdn) {
-		// 	const row = locals[cdt][cdn];
-		// 	return {
-		// 		query: "instrument.instrument.custom_instrument.purchase_receipt.purchase_receipt.get_warehouse_for_query",
-		// 		filters:{ 'parent': row.item_code }
-		// 	}
-		// });	
+		//Filter Accepted warehouse
+		frm.set_query("warehouse", "items", function(doc, cdt, cdn) {
+			const row = locals[cdt][cdn];
+			return {
+				query: "instrument.instrument.custom_instrument.purchase_receipt.purchase_receipt.get_warehouse_for_query",
+				filters:{ 'parent': row.item_code }
+			}
+		});	
 	}
 })
 frappe.ui.form.on('Purchase Receipt Item', {	
