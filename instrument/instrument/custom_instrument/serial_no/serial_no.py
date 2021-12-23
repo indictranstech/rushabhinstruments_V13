6b2bff7d60a306887fbf7de6653e6fb9927c4ccf
namrataingale <namrata.i@indictrans.in>
2021-12-23 14:34:54 +0530

    [Feature][Add week number and year in serial no,hide serial number from dn print]

--- instrument/instrument/custom_instrument/serial_no/serial_no.py
+++ instrument/instrument/custom_instrument/serial_no/serial_no.py
@@ -5,7 +5,8 @@ from frappe.utils import nowdate, cstr, flt, cint, now, getdate,get_datetime,tim
 # from datetime import date 
 from datetime import datetime   
 def autoname(doc, method):
+	# add week number and year in name
 	now = datetime.now()
 	week_no = datetime.date(now).isocalendar()[1]
-	doc.name = make_autoname('SN-' + str(week_no) + '-' + '.#####')
+	doc.name = make_autoname('SN-' + str(week_no) + '-' + str(datetime.date(now).isocalendar()[0])[2:4] + '-' + '.#####')
 	return doc.name
