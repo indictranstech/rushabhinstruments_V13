3a1db9c2f71b6081af77d145cbbefa17605d95ac
namrataingale <namrata.i@indictrans.in>
2022-05-17 09:59:39 +0530

    [Feature][Engineering REvision Customization]

--- instrument/instrument/doctype/engineering_revision/engineering_revision.py
+++ instrument/instrument/doctype/engineering_revision/engineering_revision.py
@@ -3,6 +3,13 @@
 
 import frappe
 from frappe.model.document import Document
+# from frappe.model.document import Document
+from frappe.utils.background_jobs import enqueue
+from frappe.desk.form.load import get_attachments
+from zipfile import ZipFile
+import os
+from frappe.utils import call_hook_method, cint, cstr, encode, get_files_path, get_hook_method, random_string, strip
+from frappe.model.mapper import get_mapped_doc
 
 class EngineeringRevision(Document):
 	def validate(self):
@@ -35,8 +42,9 @@ class EngineeringRevision(Document):
 			self.append('engineering_package',{
 				'engineering_package_name' : engineering_package.name
 				})
-		
-	
+		frappe.db.sql("""DELETE from `tabFile` where attached_to_doctype='Engineering Revision' and attached_to_name=%s""",
+			(self.name))
+		self.other_revisions()
 	def manage_default(self):
 		""" Uncheck others if current one is selected as default or
 			check the current one as default if it the only doc for the selected item
@@ -52,5 +60,97 @@ class EngineeringRevision(Document):
 		else:
 			frappe.db.set(self, "is_default", 0)
 
+	def other_revisions(self):
+		if self.other_engineering_revision:
+			for row in self.other_engineering_revision:
+				if row.use_specific_engineering_revision:
+					er = frappe.get_doc("Engineering Revision",row.revision)
+				else:
+					er_name = frappe.db.get_value("Engineering Revision",{'item_code':row.item_code,'is_default':1},'name')
+					if er_name:
+						er = frappe.get_doc("Engineering Revision",er_name)
+				if er:
+					if not row.exclude_purchasing_package:
+						purchasing_package = frappe.db.sql("""SELECT purchasing_package_name from `tabPurchasing Package Table` a join `tabEngineering Revision` b on a.parent = b.name where b.name = '{0}'""".format(er.name),as_dict=1)
+						purchasing_package_list = [item.purchasing_package_name for item in purchasing_package]
+						for col in purchasing_package_list:
+							package_doc = frappe.get_doc("Package Document",col)
+							"""Copy attachments from `package document`"""
+							from frappe.desk.form.load import get_attachments
+							attachments = get_attachments(package_doc.doctype, package_doc.name)
+							file_path = os.path.realpath(get_files_path(is_private=1))
+							# full_path = path+"/"+sitename+"private/files/"+row.engineering_revision+"_"+doc.name+".zip"
+							full_path = file_path+ "/"+er.name+"_"+"purchasing_package"+self.name+".zip"
+							file_name = er.name+"_"+"purchasing_package"+self.name+".zip"
+							file_url = '/private/files/'+file_name
+							if len(attachments) > 0 :
+								with ZipFile(full_path,'w') as zip:
+									for i in attachments:
+										file_doc = frappe.get_doc("File",{'file_url':i.file_url})
+										full_file_path = file_doc.get_full_path()
+										zip.write(full_file_path)
+								file_doc = frappe.new_doc("File")
+								file_doc.file_name =file_name
+								file_doc.folder = "Home/Attachments"
+								file_doc.attached_to_doctype = self.doctype
+								file_doc.attached_to_name = self.name
+								file_doc.file_url = file_url
+								file_doc.insert(ignore_permissions=True)
+								frappe.db.commit()
+								# doc.reload()
+					if not row.exclude_manufacturing_package:
+						manufacturing_package = frappe.db.sql("""SELECT manufacturing_package_name from `tabManufacturing Package Table` a join `tabEngineering Revision` b on a.parent = b.name where b.name = '{0}'""".format(er.name),as_dict=1)
+						manufacturing_package_list = [item.manufacturing_package_name for item in manufacturing_package]
+						for col in manufacturing_package_list:
+							package_doc = frappe.get_doc("Package Document",col)
+							"""Copy attachments from `package document`"""
+							from frappe.desk.form.load import get_attachments
+							attachments = get_attachments(package_doc.doctype, package_doc.name)
+							file_path = os.path.realpath(get_files_path(is_private=1))
+							# full_path = path+"/"+sitename+"private/files/"+row.engineering_revision+"_"+doc.name+".zip"
+							full_path = file_path+ "/"+er.name+"_"+"manufacturing_package"+self.name+".zip"
+							file_name = er.name+"_"+"manufacturing_package"+self.name+".zip"
+							file_url = '/private/files/'+file_name
+							if len(attachments) > 0 :
+								with ZipFile(full_path,'w') as zip:
+									for i in attachments:
+										file_doc = frappe.get_doc("File",{'file_url':i.file_url})
+										full_file_path = file_doc.get_full_path()
+										zip.write(full_file_path)
+								file_doc = frappe.new_doc("File")
+								file_doc.file_name =file_name
+								file_doc.folder = "Home/Attachments"
+								file_doc.attached_to_doctype = self.doctype
+								file_doc.attached_to_name = self.name
+								file_doc.file_url = file_url
+								file_doc.insert(ignore_permissions=True)
+								frappe.db.commit()
+					if not row.exclude_engineering_package:
+						engineering_package = frappe.db.sql("""SELECT engineering_package_name from `tabEngineering Package Table` a join `tabEngineering Revision` b on a.parent = b.name where b.name = '{0}'""".format(er.name),as_dict=1)
+						engineering_package_list = [item.engineering_package_name for item in engineering_package]
+						for col in engineering_package_list:
+							package_doc = frappe.get_doc("Package Document",col)
+							"""Copy attachments from `package document`"""
+							from frappe.desk.form.load import get_attachments
+							attachments = get_attachments(package_doc.doctype, package_doc.name)
+							file_path = os.path.realpath(get_files_path(is_private=1))
+							# full_path = path+"/"+sitename+"private/files/"+row.engineering_revision+"_"+doc.name+".zip"
+							full_path = file_path+ "/"+er.name+"_"+"engineering_package"+self.name+".zip"
+							file_name = er.name+"_"+"engineering_package"+self.name+".zip"
+							file_url = '/private/files/'+file_name
+							if len(attachments) > 0 :
+								with ZipFile(full_path,'w') as zip:
+									for i in attachments:
+										file_doc = frappe.get_doc("File",{'file_url':i.file_url})
+										full_file_path = file_doc.get_full_path()
+										zip.write(full_file_path)
+								file_doc = frappe.new_doc("File")
+								file_doc.file_name =file_name
+								file_doc.folder = "Home/Attachments"
+								file_doc.attached_to_doctype = self.doctype
+								file_doc.attached_to_name = self.name
+								file_doc.file_url = file_url
+								file_doc.insert(ignore_permissions=True)
+								frappe.db.commit()	
 	def on_trash(self):
 		frappe.db.sql("""delete from `tabPackage Document` where item_code = '{0}' and revision ='{1}'""".format(self.item_code,self.revision))
